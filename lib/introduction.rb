63aa6911baee863b84d8dd8311cf2d595419a952
Allie <alliecaton3@gmail.com>
2020-12-07 13:38:07 -0500

    commit tree a041543c10718501da87297cd928fbb64d11c0b1

---
+++ lib/introduction.rb
@@ -0,0 +1,15 @@
+#intro
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+end
+
+introduction("Josh")
+
+#intro with language
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("Dan", "Ember.js")
